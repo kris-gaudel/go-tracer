6739e3bee36e94db2695283a046cb783f29a61a4
Kristofer <kristofergaudel@gmail.com>
2024-09-02 14:53:43 -0400

    Merge branch 'master' of https://github.com/kris-gaudel/go-tracer

--- main.go
+++ main.go
@@ -3,49 +3,64 @@ package main
 import (
 	"go-tracer/src/camera"
 	"go-tracer/src/hittable"
-	"go-tracer/src/utils"
 	"go-tracer/src/vec3"
 )
 
 func main() {
 	// World
 	var world hittable.HittableList
-	ground_material := hittable.Lambertian{Albedo: vec3.Vec3{X: 0.5, Y: 0.5, Z: 0.5}}
-	sphereOne := hittable.Sphere{Center: vec3.Vec3{X: 0, Y: -1000, Z: 0}, Radius: 1000, Mat: ground_material}
-	world.Append(&sphereOne)
+	material_ground := hittable.Lambertian{Albedo: vec3.Vec3{X: 0.8, Y: 0.8, Z: 0.0}}
+	material_center := hittable.Lambertian{Albedo: vec3.Vec3{X: 0.1, Y: 0.2, Z: 0.5}}
+	material_left := hittable.Dielectric{Ir: 1.5}
+	material_right := hittable.Metal{Albedo: vec3.Vec3{X: 0.8, Y: 0.6, Z: 0.2}, Fuzz: 0.0}
 
-	for a := -11; a < 11; a++ {
-		for b := -11; b < 11; b++ {
-			choose_mat := utils.RandomDouble()
-			center := vec3.Point3{X: float64(a) + 0.9*utils.RandomDouble(), Y: 0.2, Z: float64(b) + 0.9*utils.RandomDouble()}
+	sphereOne := hittable.Sphere{Center: vec3.Point3{X: 0, Y: -100.5, Z: -1}, Radius: 100, Mat: material_ground}
+	sphereTwo := hittable.Sphere{Center: vec3.Point3{X: 0, Y: 0, Z: -1}, Radius: 0.5, Mat: material_center}
+	sphereThree := hittable.Sphere{Center: vec3.Point3{X: -1, Y: 0, Z: -1}, Radius: 0.5, Mat: material_left}
+	sphereFour := hittable.Sphere{Center: vec3.Point3{X: -1, Y: 0, Z: -1}, Radius: -0.4, Mat: material_left}
+	sphereFive := hittable.Sphere{Center: vec3.Point3{X: 1, Y: 0, Z: -1}, Radius: 0.5, Mat: material_right}
+	world.Append(sphereOne)
+	world.Append(sphereTwo)
+	world.Append(sphereThree)
+	world.Append(sphereFour)
+	world.Append(sphereFive)
+	// var world hittable.HittableList
+	// ground_material := hittable.Lambertian{Albedo: vec3.Vec3{X: 0.5, Y: 0.5, Z: 0.5}}
+	// sphereOne := hittable.Sphere{Center: vec3.Vec3{X: 0, Y: -1000, Z: 0}, Radius: 1000, Mat: ground_material}
+	// world.Append(&sphereOne)
 
-			if ((center.Subtract(vec3.Point3{X: 4, Y: 0.2, Z: 0})).Length()) > 0.9 {
-				var sphere_material hittable.Material
-				if choose_mat < 0.8 {
-					albedo := vec3.Vec3{X: utils.RandomDouble(), Y: utils.RandomDouble(), Z: utils.RandomDouble()}
-					sphere_material = hittable.Lambertian{Albedo: albedo}
-					world.Append(&hittable.Sphere{Center: center, Radius: 0.2, Mat: sphere_material})
-				} else if choose_mat < 0.95 {
-					albedo := vec3.Vec3{X: utils.RandomDoubleRange(0.5, 1), Y: utils.RandomDoubleRange(0.5, 1), Z: utils.RandomDoubleRange(0.5, 1)}
-					fuzz := utils.RandomDoubleRange(0, 0.5)
-					sphere_material = hittable.Metal{Albedo: albedo, Fuzz: fuzz}
-					world.Append(&hittable.Sphere{Center: center, Radius: 0.2, Mat: sphere_material})
-				} else {
-					sphere_material = hittable.Dielectric{Ir: 1.5}
-					world.Append(&hittable.Sphere{Center: center, Radius: 0.2, Mat: sphere_material})
-				}
-			}
-		}
-	}
+	// for a := -11; a < 11; a++ {
+	// 	for b := -11; b < 11; b++ {
+	// 		choose_mat := utils.RandomDouble()
+	// 		center := vec3.Point3{X: float64(a) + 0.9*utils.RandomDouble(), Y: 0.2, Z: float64(b) + 0.9*utils.RandomDouble()}
 
-	materialOne := hittable.Dielectric{Ir: 1.5}
-	world.Append(&hittable.Sphere{Center: vec3.Point3{X: 0, Y: 1, Z: 0}, Radius: 1.0, Mat: materialOne})
+	// 		if ((center.Subtract(vec3.Point3{X: 4, Y: 0.2, Z: 0})).Length()) > 0.9 {
+	// 			var sphere_material hittable.Material
+	// 			if choose_mat < 0.8 {
+	// 				albedo := vec3.Vec3{X: utils.RandomDouble(), Y: utils.RandomDouble(), Z: utils.RandomDouble()}
+	// 				sphere_material = hittable.Lambertian{Albedo: albedo}
+	// 				world.Append(&hittable.Sphere{Center: center, Radius: 0.2, Mat: sphere_material})
+	// 			} else if choose_mat < 0.95 {
+	// 				albedo := vec3.Vec3{X: utils.RandomDoubleRange(0.5, 1), Y: utils.RandomDoubleRange(0.5, 1), Z: utils.RandomDoubleRange(0.5, 1)}
+	// 				fuzz := utils.RandomDoubleRange(0, 0.5)
+	// 				sphere_material = hittable.Metal{Albedo: albedo, Fuzz: fuzz}
+	// 				world.Append(&hittable.Sphere{Center: center, Radius: 0.2, Mat: sphere_material})
+	// 			} else {
+	// 				sphere_material = hittable.Dielectric{Ir: 1.5}
+	// 				world.Append(&hittable.Sphere{Center: center, Radius: 0.2, Mat: sphere_material})
+	// 			}
+	// 		}
+	// 	}
+	// }
 
-	materialTwo := hittable.Lambertian{Albedo: vec3.Vec3{X: 0.4, Y: 0.2, Z: 0.1}}
-	world.Append(&hittable.Sphere{Center: vec3.Point3{X: -4, Y: 1, Z: 0}, Radius: 1.0, Mat: materialTwo})
+	// materialOne := hittable.Dielectric{Ir: 1.5}
+	// world.Append(&hittable.Sphere{Center: vec3.Point3{X: 0, Y: 1, Z: 0}, Radius: 1.0, Mat: materialOne})
 
-	materialThree := hittable.Metal{Albedo: vec3.Vec3{X: 0.7, Y: 0.6, Z: 0.5}, Fuzz: 0.0}
-	world.Append(&hittable.Sphere{Center: vec3.Point3{X: 4, Y: 1, Z: 0}, Radius: 1.0, Mat: materialThree})
+	// materialTwo := hittable.Lambertian{Albedo: vec3.Vec3{X: 0.4, Y: 0.2, Z: 0.1}}
+	// world.Append(&hittable.Sphere{Center: vec3.Point3{X: -4, Y: 1, Z: 0}, Radius: 1.0, Mat: materialTwo})
+
+	// materialThree := hittable.Metal{Albedo: vec3.Vec3{X: 0.7, Y: 0.6, Z: 0.5}, Fuzz: 0.0}
+	// world.Append(&hittable.Sphere{Center: vec3.Point3{X: 4, Y: 1, Z: 0}, Radius: 1.0, Mat: materialThree})
 
 	// Camera
 	var cam camera.Camera
